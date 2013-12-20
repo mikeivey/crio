f0efbb123710febf0c84351a1e4432b34abcd471
Mike Ivey <mike@modernmsg.com>
2013-12-19 21:05:32 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crio::Application.config.secret_key_base = '3629cbbf4e31d39a738f3b1f7282a5e138d2b000c2d940696670f600071d3dae185ae4a64e34749de43eb7a21f1634aec617ef43b29e86f30057de7c848cf044'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Crio::Application.config.session_store :cookie_store, key: '_crio_session'
